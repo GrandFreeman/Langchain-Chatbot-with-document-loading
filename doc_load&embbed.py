5600dfc1e7aa7b412feace672bfd42b4894bd289
GrandFreeman <54396083+GrandFreeman@users.noreply.github.com>
2026-01-30 13:01:45 +0800

    Update doc_load&embbed.py

--- doc_load&embbed.py
+++ doc_load&embbed.py
@@ -1,5 +1,4 @@
 from ibm_watsonx_ai.foundation_models import ModelInference
-from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
 from ibm_watsonx_ai.metanames import EmbedTextParamsMetaNames
 from ibm_watsonx_ai import Credentials
 
