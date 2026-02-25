31edd448899e35c39b2fa73bba3d42bbe117038b
GrandFreeman <54396083+GrandFreeman@users.noreply.github.com>
2026-02-25 16:07:56 +0800

    Update qabot.py

--- qabot.py
+++ qabot.py
@@ -51,7 +51,7 @@ def get_llm():
 
 def retriever(file): ## We can consider work with ParentDocumentRetriever.
     if file is None:
-        raise ValueError("No file uploaded")
+        return None
     parent_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=100,
